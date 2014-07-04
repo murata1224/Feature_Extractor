8137246d5bc7b51ecd782f93d8849dc7a0f2362e
Yuya Murata <murata1224@gmail.com>
2014-07-04 18:51:55 +0900

    Revise feature_extractor.rb

--- feature_extractor.rb
+++ feature_extractor.rb
@@ -11,23 +11,22 @@ end
 ics_string = File.open(filename)
 ical_extractor = IcsExtractor.new(ics_string)
 
-puts "### calendar ###"
-
-puts "summary_average:\t " +
-  ical_extractor.summary_average.round(1).to_s
-puts "event_count:\t\t " +
-  ical_extractor.event_count.to_s
+puts "\n### Calendar ###"
 cal_start, cal_end = ical_extractor.duration_of_using_calendar
 puts "duration:\t\t" + cal_start.to_s + "," + cal_end.to_s
-puts "days of duration:\t" + (cal_end - cal_start).to_s
+puts "num of days in duration:" + (cal_end - cal_start).to_i.to_s
+puts "num of events:\t\t" +
+  ical_extractor.event_count.to_s
+puts "AVG of summary's chars:\t" +
+  ical_extractor.summary_average.round(1).to_s
 
-puts "### event ###"
-puts "average_all_events\t:" +
-  ical_extractor.average_all_events_to_dtstart.to_s
-puts "average_genuine_events\t:" +
-  ical_extractor.average_genuine_events_to_dtstart.to_s
+puts "\n### Event ###"
+puts "AVG of days until expected date in all events:\t\t" +
+  ical_extractor.average_all_events_to_dtstart.round(2).to_s
+puts "AVG of days until expected date in genuine events:\t" +
+  ical_extractor.average_genuine_events_to_dtstart.round(2).to_s
 
-puts "distribution of days from CREATED to DTSTART"
+puts "distribution of days from CREATED to DTSTART:"
 puts "|more before\t|0d\t|1d\t|1w\t|2w\t|1m\t|2m\t|3m\t|6m\t|more after\t|"
 puts "|" + ical_extractor.events_held_within(-999,-1).count.to_s + "\t\t|" +
   ical_extractor.events_held_within(-1,  0).count.to_s + "\t|" +
