524cf1fa0d7d3464585cd6096f0ee0af18c3a48e
Yuya Murata <murata1224@gmail.com>
2014-07-04 18:28:05 +0900

    Use instance variables of Date Class

--- icalendar_extractor.rb
+++ icalendar_extractor.rb
@@ -31,6 +31,7 @@ class IcsExtractor
 
   def initialize(ics_string)
     @calendar = Icalendar.parse(ics_string).first
+    set_my_variables
   end
 
   def event_count
@@ -46,30 +47,43 @@ class IcsExtractor
   end
 
 
-  def average_days_to_dtstart
-    distances = distances_from_created_to_dtstart
+  # 事前に登録された(dtstart - created がプラス)eventの
+  # CREATEDからDTSTARTまでの平均日数
+  def average_genuine_events_to_dtstart
+    distances = Array.new
+    @calendar.events.each do |e|
+      if (e.my_dtstart - e.my_created) > 0
+        distances << (e.my_dtstart - e.my_created).to_i
+      end
+    end
     return distances.average
   end
 
-  # 予定作成からdays以内に実施される予定の数
-  def num_of_events_held_within(days)
-    distances = distances_from_created_to_dtstart
-    return (distances.select {|d| d <= days }).count
+  # 予定作成から from_day から to_day の間に実施される予定
+  # from_dayが-999なら負方向に無限，
+  # to_dayが999なら正方向に無限
+  def events_held_within(from_day = -999, to_day = 999)
+    @calendar.events.select do |e|
+      (from_day == -999 && to_day === 999) ||
+      (from_day == -999 && 
+       (e.my_dtstart - e.my_created) <= to_day ) ||
+        (to_day == 999 && 
+         (e.my_dtstart - e.my_created) > from_day ) ||
+        ((e.my_dtstart - e.my_created) > from_day &&
+         (e.my_dtstart - e.my_created) <= to_day )
+    end
+  end
   end
 
   private
-  def distances_from_created_to_dtstart
-    distances = Array.new
+
+  def set_my_variables
     @calendar.events.each do |e|
-      # iCalendar::Values::DateTime　クラスは，
-      # なぜかオブジェクト同士の加減算ができない
-      dtstart =
-        Date.new(e.dtstart.year, e.dtstart.month, e.dtstart.day)
-      created =
+      e.my_created =
         Date.new(e.created.year, e.created.month, e.created.day)
-      distances << (dtstart - created).to_i
+      e.my_dtstart =
+        Date.new(e.dtstart.year, e.dtstart.month, e.dtstart.day)
     end
-    return distances
   end
 
 end
